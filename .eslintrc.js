5e1058bc94dda29d681b2562eaa308d685a32bff
Tino Sabetta <tino.sabetta@gmail.com>
2024-09-08 14:26:46 +0200

    better eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,4 +10,12 @@ module.exports = {
         "ecmaVersion": "latest",
         "sourceType": "module"
     },
-}
+    "globals": {
+        "app": "readonly" // Beispiel für globale Variable
+    },
+    "rules": {
+        "semi": ["error", "always"], // Erzwingt Semikolons
+        "quotes": ["error", "double"], // Erzwingt doppelte Anführungszeichen
+        "indent": ["error", 4], // Erzwingt eine Einrückung von 2 Leerzeichen
+    }
+};
