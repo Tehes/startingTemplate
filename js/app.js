e788a86cae63b58f22418906f0fb39221513752d
Tino Sabetta <tino.sabetta@gmail.com>
2024-09-12 22:16:35 +0200

    Handle only GET requests with cache-first strategy in service worker

--- js/app.js
+++ js/app.js
@@ -29,14 +29,30 @@ window.app = {
 window.app.init();
 
 /* --------------------------------------------------------------------------------------------------
-Service Worker registration. Only use, when you want a PWA
+Service Worker configuration. Toggle 'useServiceWorker' to enable or disable the Service Worker.
 ---------------------------------------------------------------------------------------------------*/
-// if ('serviceWorker' in navigator) {
-//     window.addEventListener('load', function() {
-//         navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
-//             console.log('Service Worker registered with scope:', registration.scope);
-//         }).catch(function(error) {
-//             console.log('Service Worker registration failed:', error);
-//         });
-//     });
-// }
+const useServiceWorker = false; // Set to "true" if you want to register the Service Worker, "false" to unregister
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+        if (useServiceWorker) {
+            // Register the Service Worker
+            navigator.serviceWorker.register("/service-worker.js").then(function(registration) {
+                console.log("Service Worker registered with scope:", registration.scope);
+            }).catch(function(error) {
+                console.log("Service Worker registration failed:", error);
+            });
+        } else {
+            // Unregister all Service Workers
+            navigator.serviceWorker.getRegistrations().then(function(registrations) {
+                for (let registration of registrations) {
+                    registration.unregister().then(function(success) {
+                        if (success) {
+                            console.log("Service Worker successfully unregistered.");
+                        }
+                    });
+                }
+            });
+        }
+    });
+}
